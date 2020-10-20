686b5b4ab9d9ec19bdbc485d9097b785768155cb
Anaka Norfleet <anakan@smackaMac.local>
2020-10-20 16:53:57 -0400

    beginnings of product view

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 
+//import Button from '../../components/Button/Button';
+
 
 export default function Home(props) {
    return (
       <div>
          <h1>Home Works</h1>
+         
       </div>
    );
 }
