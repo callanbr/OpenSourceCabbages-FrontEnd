e7fb53dc9e8ad8d83509db743802167fc2fe54e4
Anaka Norfleet <anakan@smackaMac.local>
2020-10-21 16:25:02 -0400

    about us page with placeholder images and lorem ipsum

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar(props) {
-   return (
-      <>
-         <Link to='/' className='link'>
-            Home
-         </Link>
-         <Link to='/about' className='link'>
-            About
-         </Link>
-         <Link to='/register' className='link'>
-            Register
-         </Link>
-         <Link to='/product' className='link'>
-            Product
-         </Link>
-      </>
-   );
+  return (
+    <>
+      <Link to="/" className="link">
+        Home
+      </Link>
+      <Link to="/about" className="link">
+        About
+      </Link>
+      <Link to="/register" className="link">
+        Register
+      </Link>
+      <Link to="/product" className="link">
+        Product
+      </Link>
+    </>
+  );
 }
