fef119a3c79c0d5e36940a21741c59db1489ec08
callanbr <chrisallan1992@icloud.com>
2020-10-27 13:00:46 -0700

    ability to login and see user info. Ability to logout

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,36 @@
-import React from "react";
-import "./Home.css";
-import { Title } from "../../Global";
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+import { Title } from '../../Global';
+import AuthService from '../Login/AuthService';
+
+let Auth = new AuthService();
 
 export default function Home(props) {
-  return (
-    <>
-      <Title>Home Works</Title>
-    </>
-  );
+   const [state, setState] = useState({
+      loggedIn: Auth.loggedIn(),
+      username: '',
+   });
+
+   useEffect(() => {
+      // let value = Auth.getProfile().sub;
+      if (Auth.loggedIn()) {
+         setState({
+            ...state,
+            username: Auth.getProfile().sub,
+         });
+      }
+   }, []);
+
+   const handleLogout = () => {
+      Auth.logout();
+      props.history.replace('/login');
+   };
+
+   return (
+      <>
+         <Title>Home Works</Title>
+         <h1>Welcome {state.username}!</h1>
+         <button onClick={handleLogout}>Logout</button>
+      </>
+   );
 }
