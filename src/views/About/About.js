6234d5fa8e554684554b2666e126eeca641ca76e
billyle123 <72470756+billyle123@users.noreply.github.com>
2020-11-03 06:07:36 -0800

    about billy and search bar

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -23,75 +23,89 @@ export default function About(props) {
       </Banner>
       <Container>
         <Bio>
-          <Image src="https://i.ibb.co/kXL09SP/brain.jpg" alt="Lincoln" className="image"></Image>
+          <Image
+            src="https://i.ibb.co/kXL09SP/brain.jpg"
+            alt="Lincoln"
+            className="image"
+          ></Image>
           <H2>Lincoln Gage</H2>
           <p>
-            Chupa chups macaroon sesame snaps sweet carrot cake brownie. Cotton candy powder tart gummies
-            fruitcake halvah danish. Sweet roll topping gummies dessert dessert gummi bears gummi bears sugar
-            plum. Carrot cake sugar plum soufflé cake brownie pie cake. Chocolate liquorice chocolate. Cotton
-            candy cake tootsie roll jujubes pastry icing danish. Cake gummi bears wafer candy jelly carrot
-            cake jelly beans. Powder jelly pastry. Powder carrot cake donut pie donut. Icing caramels
-            chocolate cake gummies sweet roll candy jujubes croissant. Marshmallow croissant bonbon sesame
-            snaps dragée wafer dragée. Jujubes croissant danish. Pastry jujubes carrot cake sweet dragée
-            tiramisu bear claw. Brownie wafer cake sweet roll danish carrot cake bonbon liquorice halvah.
-          </p>
-        </Bio>
-        <Bio>
-          <Image src="https://i.ibb.co/kXL09SP/brain.jpg" alt="Milton" className="image"></Image>
-          <H2>Milton Grady</H2>
-          <p>
-            Cake lemon drops candy canes tootsie roll sugar plum cookie cake cotton candy halvah. Pudding
-            biscuit topping danish liquorice carrot cake halvah. Carrot cake cake cupcake candy chocolate
-            cookie dragée marzipan cupcake. Marzipan gummies tart danish halvah topping halvah. Cupcake
-            cupcake jelly beans sweet roll powder gummi bears tart chocolate bar. Carrot cake chocolate cake
-            soufflé halvah biscuit liquorice jujubes tiramisu cheesecake. Biscuit topping ice cream powder
-            liquorice cookie pie. Chupa chups ice cream liquorice sweet candy oat cake chocolate cake sweet.
-            Dessert tiramisu dragée. Gingerbread muffin lollipop donut croissant tart sweet cookie. Oat cake
-            croissant croissant tiramisu tootsie roll. Carrot cake cookie candy canes chocolate macaroon
-            halvah tiramisu tiramisu. Macaroon cake macaroon candy canes.
+            Chupa chups macaroon sesame snaps sweet carrot cake brownie. Cotton
+            candy powder tart gummies fruitcake halvah danish. Sweet roll
+            topping gummies dessert dessert gummi bears gummi bears sugar plum.
+            Carrot cake sugar plum soufflé cake brownie pie cake. Chocolate
+            liquorice chocolate. Cotton candy cake tootsie roll jujubes pastry
+            icing danish. Cake gummi bears wafer candy jelly carrot cake jelly
+            beans. Powder jelly pastry. Powder carrot cake donut pie donut.
+            Icing caramels chocolate cake gummies sweet roll candy jujubes
+            croissant. Marshmallow croissant bonbon sesame snaps dragée wafer
+            dragée. Jujubes croissant danish. Pastry jujubes carrot cake sweet
+            dragée tiramisu bear claw. Brownie wafer cake sweet roll danish
+            carrot cake bonbon liquorice halvah.
           </p>
         </Bio>
 
         <Bio>
-          <Image src="https://i.ibb.co/kXL09SP/brain.jpg" alt="Billy" className="image"></Image>
+          <Image
+            src="https://i.ibb.co/q5tz22J/123677790-715174182710362-6615206298440617095-n.jpg"
+            alt="Billy"
+            className="image"
+          ></Image>
           <H2>Billy Le</H2>
           <p>
-            Gummies pastry macaroon donut cake jelly marzipan powder. Chupa chups dessert chocolate jelly
-            muffin donut candy wafer. Cheesecake carrot cake cheesecake toffee jelly-o marzipan tart. Icing
-            marzipan tootsie roll. Chocolate topping gummi bears fruitcake gummies. Tootsie roll macaroon
-            fruitcake cotton candy jujubes tart chocolate bar gummies sweet. Chocolate fruitcake wafer
-            cheesecake bonbon cotton candy ice cream fruitcake powder. Liquorice candy canes carrot cake.
-            Tiramisu tootsie roll croissant topping gummi bears donut oat cake tootsie roll. Cheesecake sugar
-            plum jelly beans candy canes marshmallow gummies. Caramels croissant bonbon cheesecake muffin.
-            Danish tiramisu tootsie roll. Ice cream marshmallow caramels chocolate cake gummies.
+            What sets me apart than other applicants is my willingness to learn,
+            and grow from each experience. Upon seeing each experience you will
+            see the applicant utilizing a mix of each end every encounter to
+            ensure that their employers and customers are given top quality work
+            and consideration. His skill will include out of the box thinking,
+            excellent time utilization, and teamwork. Though being able to
+            approach problems and solutions on my own. Teamwork and comradarie
+            is a very important value to me. Developing more positive relations
+            with teammates and customers is one of the core parts of making a
+            more productive and safe workspace. If anything would set me apart
+            from other applicants would be my ability to become a team player as
+            well as utilizing most if not all prior experiences to ensure top
+            quality work.
           </p>
         </Bio>
 
         <Bio>
-          <Image src="https://i.ibb.co/CQtwZWJ/Anakaheadshot.jpg" alt="Anaka" className="image"></Image>
+          <Image
+            src="https://i.ibb.co/CQtwZWJ/Anakaheadshot.jpg"
+            alt="Anaka"
+            className="image"
+          ></Image>
           <H2>Anaka Norfleet</H2>
           <p>
-            What sets me apart from everyone else stems from my background in nursing. During my 14+ years as
-            an ER nurse, I have honed my communication skills by greeting people on one of the worst days of
-            their lives, calming their fears and building their trust by employing active listening and then
-            using the information obtained to combine with my technical skills to not only find a solution
-            that meets their needs, but also allows me to anticipate needs they may not even know they have
-            yet. I believe the key components of customer service include not only listening to the customer,
-            but knowing what questions to ask in order to dig a little deeper. What a person perceives as
-            their biggest problem is often not the root of the actual problem. My process of extracting
-            information, using critical thinking to evaluate and prioritize what’s important and then
-            employing the necessary technical skills to solve the problem is what enables me to efficiently
-            create a solution to whatever problem a customer may experience.
+            What sets me apart from everyone else stems from my background in
+            nursing. During my 14+ years as an ER nurse, I have honed my
+            communication skills by greeting people on one of the worst days of
+            their lives, calming their fears and building their trust by
+            employing active listening and then using the information obtained
+            to combine with my technical skills to not only find a solution that
+            meets their needs, but also allows me to anticipate needs they may
+            not even know they have yet. I believe the key components of
+            customer service include not only listening to the customer, but
+            knowing what questions to ask in order to dig a little deeper. What
+            a person perceives as their biggest problem is often not the root of
+            the actual problem. My process of extracting information, using
+            critical thinking to evaluate and prioritize what’s important and
+            then employing the necessary technical skills to solve the problem
+            is what enables me to efficiently create a solution to whatever
+            problem a customer may experience.
           </p>
           <p>
-            During my time as an ER nurse, I have been challenged many times to think outside the box in order
-            to treat unique situations that people sometimes find themselves in. Never knowing what is coming
-            through the door at any moment has prepared me to remain calm and think clearly under extremely
-            stressful situations. Critical thinking coupled with creativity is the basis of ER nursing.
+            During my time as an ER nurse, I have been challenged many times to
+            think outside the box in order to treat unique situations that
+            people sometimes find themselves in. Never knowing what is coming
+            through the door at any moment has prepared me to remain calm and
+            think clearly under extremely stressful situations. Critical
+            thinking coupled with creativity is the basis of ER nursing.
           </p>
           <p>
-            I believe this combination of skills is not only rare to find in someone else, but it also
-            extremely valuable to have in a software engineer.
+            I believe this combination of skills is not only rare to find in
+            someone else, but it also extremely valuable to have in a software
+            engineer.
           </p>
         </Bio>
       </Container>
--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -2,12 +2,24 @@ import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { url, Title, Image, EachProduct, Description, Price, ProductName, Container } from "../../Global";
+import {
+  url,
+  Title,
+  Image,
+  EachProduct,
+  Description,
+  Price,
+  ProductName,
+  Container,
+} from "../../Global";
 import styled from "styled-components";
 import "./Product.css";
 
 export default function Product(props) {
   const [products, setProducts] = useState([]);
+  const [state, setState] = useState({
+    query: "",
+  });
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -17,15 +29,49 @@ export default function Product(props) {
     fetchProducts();
   }, []);
 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
+  };
+  const updateEventHandler = (event) => {
+    let matching = [];
+    for (let i in products) {
+      if (
+        products[i].productName
+          .toLowerCase()
+          .includes(state.query.toLowerCase())
+      ) {
+        matching.push(products[i]);
+      }
+    }
+    setProducts(matching);
+  };
+  const resetPageHandler = (event) => {
+    window.location.reload();
+  };
+
   const Stock = styled.section`
     float: right;
     padding: 10px;
     color: #aa2a31;
-  `;
+  `; 
 
   return (
     <>
       <Container>
+        <input
+          name="query"
+          placeholder="Search by Name"
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit" onClick={updateEventHandler}>
+          Go!
+        </button>
+        <button onClick={resetPageHandler}>Clear Search!</button>
         <Title>Products Page</Title>
         {products.map((product) => (
           <EachProduct key={product.productId}>
